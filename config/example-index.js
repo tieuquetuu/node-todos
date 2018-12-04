3f4375936147ef8f50b898697688ac10bdc1a92c
tieuquetuu <anhvanvan1102@gmail.com>
2018-12-04 18:20:53 +0700

    commit this app without /config

--- config/example-index.js
+++ config/example-index.js
@@ -3,6 +3,6 @@ var configValues = require('./config');
 
 module.exports = {
     getDbConnectionString : function () {
-        return `mongodb://<dbuser>:<dbpassword>@ds123844.mlab.com:23844/nodetodos`;
+        return `the url mongo`;
     }
 };
